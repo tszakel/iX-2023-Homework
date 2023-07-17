4d2f6e9582e62b90a7133125fb2d3c29c6a18f46
tszakel <45600503+tszakel@users.noreply.github.com>
2023-07-17 13:48:37 +0200

    taskList rename and radio btn

--- week_1/day_5/app.js
+++ week_1/day_5/app.js
@@ -1,108 +1,107 @@
 class Task {
-    constructor(task) {
-      this.task = task;
-    }
-
-    static fromJSON(json) {
-      return new Task(json.task);
-    }
+  constructor(taskName) {
+    this.taskName = taskName;
   }
+
+  static fromJSON(json) {
+    return new Task(json.taskName);
+  }
+}
 
 class UI {
-    constructor() {  
-      this.form = document.getElementById('form');
-      this.task = document.getElementById('task');
-  
-      this.tableBody = document.getElementById('table-body');
-  
-      this.form.addEventListener('submit', (e) => this.onFormSubmit(e));
-  
-      this.tasks = [];
+  constructor() {  
+    this.form = document.getElementById('form');
+    this.taskName = document.getElementById('task');
 
-      this.loadTasksFromLocalStorage();
-      this.renderTaskTable();
-    }
-  
-    onFormSubmit(e) {
-      e.preventDefault();
-  
-      if (this.task.value == '') {
-        return;
-      }
-  
-      const task = new Task(this.task.value);
-  
-      this.tasks.push(task);
-      this.saveTasksToLocalStorage();
-      this.renderTaskTable();
-  
-      this.task.value = '';
-    }
+    this.tableBody = document.getElementById('table-body');
 
-  renderTaskTable() {
-      this.tableBody.innerHTML = '';
-  
-      for (let i = 0; i < this.tasks.length; i++) {
-        const task = this.tasks[i];
-        console.log(this.tasks[i].task);
-  
-        const tr = this.createTaskTableRow(task);
-        this.tableBody.appendChild(tr);
-      }
-  }
+    this.form.addEventListener('submit', (e) => this.onFormSubmit(e));
 
-  createTaskTableRow(task) {
-    const tr = document.createElement('tr');
+    this.taskList = [];
 
-    const tdTask = document.createElement('td');
-    const tdComplete = document.createElement('td');
-    const tdActions = document.createElement('td');
-    
-    tdTask.innerHTML = task.task;
-    
-    const actionButtons = this.createActionButtons();
-    tdComplete.appendChild(actionButtons[0]);
-    tdActions.appendChild(actionButtons[1]);
-
-    tr.appendChild(tdTask);
-    tr.appendChild(tdComplete);
-    tr.appendChild(tdActions);
-
-    return tr;
-  }
-
-  onRemoveTaskClicked() {
-    this.tasks = this.tasks.filter((x) => {return task.task});
-
-    this.saveTasksToLocalStorage();
+    this.loadTasksFromLocalStorage();
     this.renderTaskTable();
   }
 
-  createActionButtons(task) {
-    const deleteButton = document.createElement('button');
-    const completeButton = document.createElement('radio');
+  onFormSubmit(e) {
+    e.preventDefault();
 
-    deleteButton.setAttribute('class', 'btn btn-danger btn-sm');
-    deleteButton.innerHTML = 'Delete';
-    deleteButton.addEventListener('click', () => { this.onRemoveTaskClicked(task); });
-
-    completeButton.setAttribute('class', 'form-check-input');
-
-    return [completeButton,deleteButton];
-  }
-
-  saveTasksToLocalStorage (){
-    const json = JSON.stringify(this.tasks);
-    localStorage.setItem('tasks', json);
-  }
-
-  loadTasksFromLocalStorage (){
-    const json = localStorage.getItem('tasks');
-    if(json) {
-      const taskArr = JSON.parse(json);
-      this.taskArr = taskArr.map((x) => Task.fromJSON(x));
+    if (this.taskName.value == '') {
+      return;
     }
+
+    const task = new Task(this.taskName.value);
+
+    this.taskList.push(task);
+    this.saveTasksToLocalStorage();
+    this.renderTaskTable();
+
+    this.taskName.value = '';
   }
+
+renderTaskTable() {
+    this.tableBody.innerHTML = '';
+
+    for (let i = 0; i < this.taskList.length; i++) {
+      const task = this.taskList[i];
+
+      const tr = this.createTaskTableRow(task);
+      this.tableBody.appendChild(tr);
+    }
+}
+
+createTaskTableRow(task) {
+  const tr = document.createElement('tr');
+
+  const tdTask = document.createElement('td');
+  const tdComplete = document.createElement('td');
+  const tdActions = document.createElement('td');
+  
+  tdTask.innerHTML = task.taskName;
+  
+  const actionButtons = this.createActionButtons();
+  tdComplete.appendChild(actionButtons[0]);
+  tdActions.appendChild(actionButtons[1]);
+
+  tr.appendChild(tdTask);
+  tr.appendChild(tdComplete);
+  tr.appendChild(tdActions);
+
+  return tr;
+}
+
+onRemoveTaskClicked(task) {
+  this.taskList = this.taskList.filter((curTask) => {return task.taskName != curTask.taskName;});
+
+  this.saveTasksToLocalStorage();
+  this.renderTaskTable();
+}
+
+createActionButtons(task) {
+  const deleteButton = document.createElement('button');
+  const completeButton = document.createElement('input');
+
+  deleteButton.setAttribute('class', 'btn btn-danger btn-sm');
+  deleteButton.innerHTML = 'Delete';
+  deleteButton.addEventListener('click', () => { this.onRemoveTaskClicked(task); });
+
+  completeButton.setAttribute('type', 'radio');
+
+  return [completeButton,deleteButton];
+}
+
+saveTasksToLocalStorage (){
+  const json = JSON.stringify(this.taskList);
+  localStorage.setItem('tasks', json);
+}
+
+loadTasksFromLocalStorage (){
+  const json = localStorage.getItem('tasks');
+  if(json) {
+    const taskArr = JSON.parse(json);
+    this.taskArr = taskArr.map((x) => Task.fromJSON(x));
+  }
+}
 
 }
 
