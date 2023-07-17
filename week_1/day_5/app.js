b963d1816ae000c2fd808682ce7375dae8919286
tszakel <45600503+tszakel@users.noreply.github.com>
2023-07-17 11:57:42 +0200

    task list updated

--- week_1/day_5/app.js
+++ week_1/day_5/app.js
@@ -2,6 +2,10 @@ class Task {
     constructor(task) {
       this.task = task;
     }
+
+    static fromJSON(json) {
+      return new Task(json.task);
+    }
   }
 
 class UI {
@@ -15,12 +19,12 @@ class UI {
   
       this.tasks = [];
 
+      this.loadTasksFromLocalStorage();
       this.renderTaskTable();
     }
   
     onFormSubmit(e) {
       e.preventDefault();
-      console.log('added');
   
       if (this.task.value == '') {
         return;
@@ -29,6 +33,7 @@ class UI {
       const task = new Task(this.task.value);
   
       this.tasks.push(task);
+      this.saveTasksToLocalStorage();
       this.renderTaskTable();
   
       this.task.value = '';
@@ -39,6 +44,7 @@ class UI {
   
       for (let i = 0; i < this.tasks.length; i++) {
         const task = this.tasks[i];
+        console.log(this.tasks[i].task);
   
         const tr = this.createTaskTableRow(task);
         this.tableBody.appendChild(tr);
@@ -49,27 +55,55 @@ class UI {
     const tr = document.createElement('tr');
 
     const tdTask = document.createElement('td');
+    const tdComplete = document.createElement('td');
     const tdActions = document.createElement('td');
     
-    tdTask.innerHTML = task.value;
+    tdTask.innerHTML = task.task;
     
     const actionButtons = this.createActionButtons();
-    tdActions.appendChild(actionButtons[0]);
+    tdComplete.appendChild(actionButtons[0]);
+    tdActions.appendChild(actionButtons[1]);
 
     tr.appendChild(tdTask);
+    tr.appendChild(tdComplete);
+    tr.appendChild(tdActions);
 
     return tr;
   }
 
-  createActionButtons() {
+  onRemoveTaskClicked() {
+    this.tasks = this.tasks.filter((x) => {return task.task});
+
+    this.saveTasksToLocalStorage();
+    this.renderTaskTable();
+  }
+
+  createActionButtons(task) {
     const deleteButton = document.createElement('button');
+    const completeButton = document.createElement('radio');
 
     deleteButton.setAttribute('class', 'btn btn-danger btn-sm');
     deleteButton.innerHTML = 'Delete';
-    deleteButton.addEventListener('click', () => { console.log('Delete button clicked'); });
+    deleteButton.addEventListener('click', () => { this.onRemoveTaskClicked(task); });
 
-    return [deleteButton];
+    completeButton.setAttribute('class', 'form-check-input');
+
+    return [completeButton,deleteButton];
   }
+
+  saveTasksToLocalStorage (){
+    const json = JSON.stringify(this.tasks);
+    localStorage.setItem('tasks', json);
+  }
+
+  loadTasksFromLocalStorage (){
+    const json = localStorage.getItem('tasks');
+    if(json) {
+      const taskArr = JSON.parse(json);
+      this.taskArr = taskArr.map((x) => Task.fromJSON(x));
+    }
+  }
+
 }
 
 const ui = new UI();
